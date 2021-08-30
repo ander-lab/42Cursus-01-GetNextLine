ad2379a5fb399f829c6e47048d9779f7455befd0
Alex <jimenezs.alex@gmail.com>
2021-08-30 21:07:55 +0200

    get_next_line

---
+++ main.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajimenez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/30 16:49:11 by ajimenez          #+#    #+#             */
+/*   Updated: 2021/08/30 21:05:27 by ajimenez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+int	main(int ac, char **av)
+{
+	int		fd;
+	char	*line;
+	int		num;
+
+	if (ac == 2)
+		fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+		printf("Error al abrir file");
+	else
+	{
+		num = 1;
+		line = get_next_line(fd);
+		while (line)
+		{
+			printf("/* %i */%s", num, line);
+			line = get_next_line(fd);
+			num++;
+		}
+		printf("/* %i */%s", num, line);
+	}
+	close(fd);
+	return (0);
+}
