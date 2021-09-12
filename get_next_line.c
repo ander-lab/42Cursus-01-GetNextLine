53ba9823d17c0444b4d92b0fe783480f9e457594
Alex <jimenezs.alex@gmail.com>
2021-09-12 22:41:22 +0200

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ajimenez <ajimenez@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 16:46:20 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/09/09 15:29:50 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/09/12 00:26:54 by Alejandro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,48 @@ char	*ft_strdup(char *s1)
 	return (s_dup);
 }
 
-char	*get_line(char **saved, int fd, int len);
+char	*get_line(char **saved, int fd, size_t len)
+{
+	char	*line;
+	char	*tmp;
 
+	line = NULL;
+	if (saved[fd][len] != '\n')
+	{
+		line = ft_strdup(saved[fd]);
+		free(saved[fd]);
+		saved[fd] = NULL;
+	}
+	else
+	{
+		//line = ft_substr(saved[fd], 0, len + 1);
+		line = ft_strdup(&(saved[fd][len + 1]));
+		tmp = ft_strdup(&(saved[fd][len + 1]));
+		free(saved[fd]);
+		saved[fd] = tmp;
+	}
+	return(line);
+}
 
 char	*check_boom(char **saved, int fd)
 {
-	int	aux;
+	size_t	aux;
 
 	aux = 0;
 	if (!saved[fd])
 		return(0);
-	while (!saved[fd][aux])
+	while (!saved[fd][aux] && saved[fd][aux] != '\n')
 		aux++;
 	return (get_line(saved, fd, aux));
 }
 
 char	*get_next_line(int fd)
 {
-	ssize_t	chars;
-	char	*saved[FILE_N];
-	char	*tmp;
-	char	*buff[BUFFER_SIZE + 1];
+	ssize_t		chars;
+	static char	*saved[FILE_N];
+	char		*tmp;
+	char 		*buff[BUFFER_SIZE + 1];
+	char		*check;
 
 	if (fd == -1 || read(fd, buff, BUFFER_SIZE) == -1)
 		return  (0);
@@ -58,7 +79,7 @@ char	*get_next_line(int fd)
 	while (chars)
 	{
 		if (!saved[fd])
-			saved[fd] = ft_calloc(sizeof(buff));
+			saved[fd] = ft_calloc(sizeof(char), BUFFER_SIZE);
 		tmp = ft_strjoin(saved[fd], buff);
 		free(saved[fd]);
 		saved[fd] = tmp;
@@ -66,5 +87,6 @@ char	*get_next_line(int fd)
 			break ;
 		chars = read(fd, buff, BUFFER_SIZE);
 	}
-	return (0);
+	check = check_boom(saved, fd);
+	return (check);
 }
